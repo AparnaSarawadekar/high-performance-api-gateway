5edf4a348718cc4daa0c2a2042daf4127dd59894
Aparna Sarawadekar <49954855+AparnaSarawadekar@users.noreply.github.com>
2025-09-25 17:45:09 -0400

    Create k6_baseline.js

---
+++ tests/load/k6_baseline.js
@@ -0,0 +1,7 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export default function () {
+  let res = http.get('http://localhost:8000/healthz');
+  check(res, { 'status is 200': (r) => r.status === 200 });
+}
